01562055bceb2af173dec76f9b572df0715748f4
kani0798 <arstanbaevakani@gmail.com>
2022-01-28 11:13:57 +0600

    Order history.

--- order/views.py
+++ order/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 from menu.models import Product
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 
 
 def order_create(request):
@@ -21,14 +21,26 @@ def order_create(request):
             for product_id, description in cart.items():
                 product = Product.objects.get(pk=int(product_id))
                 OrderItem.objects.create(order=order,
-                                         product=product)
+                                         product=product,
+                                         quantity=int(description['quantity']))
                 print(product)
             cart.clear()
             return render(request, 'order_created.html', locals())
     else:
         order_form = OrderCreateForm()
-        prices = [float(float(item['quantity']) * float(item['price'])) for item in cart.values()]
+        prices = [float(item['quantity']) * float(item['price']) for item in cart.values()]
         total_cost = round(sum(prices), 2)
     return render(request, 'order_process.html', locals())
 
 
+def order_history(request):
+    # orders = request.user.orders.all()
+    orders = Order.objects.filter(user=request.user)
+    return render(request, 'history.html', {'orders': orders})
+
+
+def order_history_detail(request, order_id):
+    order = Order.objects.get(pk=order_id)
+    order_items = order.items.all()
+    # order_items = OrderItem.objects.filter(order=order_id)
+    return render(request, 'history_detail.html', locals())
